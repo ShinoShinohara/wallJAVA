b03ae873948e4c03c5ff0542949f3c72d4468d2f
shinoshinohara <vshinohara@outlook.com>
2022-09-08 16:52:55 -0400

    easy implementation

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import Supplies.Muro;
+
+public class Main {
+    public static void main(String[] arg) {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("Digite a largura: ");
+            double largura = scan.nextDouble();
+            System.out.print("Digite a Altura: ");
+            double altura = scan.nextDouble();
+            double area = CalcularArea(largura, altura);
+            System.out.println("Area: " + area);
+            System.out.print("Digite valor pedreiro: ");
+            double pedreiro = scan.nextDouble();
+            System.out.print("Digite valor servente: ");
+            double servente = scan.nextDouble();
+            System.out.print("Digite valor tijolos: ");
+            double tijolos = scan.nextDouble();
+            System.out.print("Digite valor argamassa: ");
+            double argamassa = scan.nextDouble();
+            double alvenaria = CalcularAlvenaria(area, pedreiro, servente, tijolos, argamassa);
+            System.out.println("Alvenaria: " + alvenaria);
+            double emboco = CalcularEmboco(area, pedreiro, servente, argamassa);
+            System.out.println("Emboco: " + emboco);
+            double custo = CalcularCusto(alvenaria, emboco);
+            System.out.println("Custo: " + custo);
+            System.out.print("Digite margem de lucro: ");
+            double margem = scan.nextDouble();
+            double lucro = CalcularLucro(margem, custo);
+            System.out.println("Lucro: " + lucro);
+        }
+    }
+    static double CalcularArea(double a, double b) {
+        return (a * b);
+    }
+
+    static double CalcularAlvenaria(double area, double pedreiro, double servente, double tijolos, double argamassa) {
+        return area * (pedreiro * 0.8 + servente * 0.8 + tijolos * 21 + argamassa * 10);
+    }
+
+    private static double CalcularEmboco(double area, double pedreiro, double servente, double argamassa) {
+        return 2 * area * (pedreiro * 0.5 + servente * 0.5 + argamassa * 40.0);
+    }
+
+    private static double CalcularCusto(double alvenaria, double emboco) {
+        return alvenaria + emboco;
+    }
+
+    private static double CalcularLucro(double margem, double custo) {
+        return custo + custo * margem / 100;
+    }
+}
